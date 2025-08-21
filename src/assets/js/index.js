c560047101dd5a54453b9c2a661751d15f24477d
alegawra <alegawra@yandex.ru>
2025-08-21 23:03:50 +0300

    main page

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,8 +7,9 @@ Swiper.use([Pagination, Navigation, Autoplay]);
 
 // Инициализация
 const swiper = new Swiper(".slider-main", {
+    autoplay: true,
     pagination: {
-        el: ".swiper-pagination",
+        el: ".slider-main .swiper-pagination",
         clickable: true
     }
 });
@@ -18,8 +19,8 @@ const swiper2 = new Swiper(".slider-brands", {
     loop: true,
     spaceBetween: 30,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.slider-brands .swiper-button-next',
+        prevEl: '.slider-brands .swiper-button-prev'
     },
 });
 
@@ -27,7 +28,33 @@ const swiper3 = new Swiper(".slider-popular", {
     slidesPerView: 4,
     spaceBetween: 30,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.slider-popular .swiper-button-next',
+        prevEl: '.slider-popular .swiper-button-prev'
+    },
+});
+
+const swiper4 = new Swiper(".slider-new-items", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.slider-new-items .swiper-button-next',
+        prevEl: '.slider-new-items .swiper-button-prev'
+    },
+});
+
+const swiper5 = new Swiper(".slider-item-series", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.slider-item-series .swiper-button-next',
+        prevEl: '.slider-item-series .swiper-button-prev'
+    },
+});
+
+const swiper6 = new Swiper(".slider-main-reviews", {
+    slidesPerView: 2,
+    navigation: {
+        nextEl: '.main-reviews__item .swiper-button-next',
+        prevEl: '.main-reviews__item .swiper-button-prev'
     },
 });
